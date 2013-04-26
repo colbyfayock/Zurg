2605de9bcf190936b9a356d9c0cc226016a4b9c8
Colby Fayock <fayock@gmail.com>
2013-04-26 15:49:44 -0400

    js

--- assets/js/lib/functions.js
+++ assets/js/lib/functions.js
@@ -1,6 +1,16 @@
+function viewport() {
+    var e = window,
+        a = 'inner';
+    if (!('innerWidth' in window)) {
+        a = 'client';
+        e = document.documentElement || document.body;
+    }
+    return { width : e[ a+'Width' ], height : e[ a+'Height' ] };
+};
+
 // Device window is < 640px
 var isSmall = function() {
-    var winW = $(window).width();
+    var winW = viewport().width;
     if (winW < 640) {
         return true;
     } else {
@@ -9,7 +19,7 @@ var isSmall = function() {
 };
 
 var clearStyles = function(e) {
-    var windowWidth = $(window).width();
+    var windowWidth = viewport().width;
     if(!isSmall()) {
         $(e).removeAttr("style");
     }
