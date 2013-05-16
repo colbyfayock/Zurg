ef0c6ba0d046021a05e49405e9d3519a1f2a7caf
Colby Fayock <fayock@gmail.com>
2013-05-16 00:03:39 -0400

    stuff

--- sample/partials/tracking.php
+++
@@ -1,7 +0,0 @@
-<!-- Google Analytics -->
-<script type="text/javascript">
-    var _gaq=[['_setAccount', 'UA-XXXXX-X'],['_trackPageview']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src='//www.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
-</script>
